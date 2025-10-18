6f983e481131eff0a5db7fa1595eb51f6b900a41
MehradMi <165255856+MehradMi@users.noreply.github.com>
2025-10-18 22:00:04 +0330

    Updated the Lead-Tracker-Extension Project: Chrome Extension

--- Lead-Tracker-Extension/js/script.js
+++ Lead-Tracker-Extension/js/script.js
@@ -1,29 +1,61 @@
 
 let leads = [];
+let leadsLocalStorageKey = "leads";
 const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
+const saveBtn = document.getElementById("save-btn");
+const saveTabBtn = document.getElementById("save-tab-btn");
+const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
 
+fetchLeads();
 
-inputBtn.addEventListener("click", function() {
+saveBtn.addEventListener("click", function() {
     leads.push(inputEl.value);
-    clearIt(inputEl);
-    console.log(leads);
-    renderLeads();
+    clearIt(inputEl); 
+    window.localStorage.setItem(leadsLocalStorageKey, JSON.stringify(leads));
+
+    render(leads);
 })
 
-function renderLeads() {
+saveTabBtn.addEventListener("click", function() {
+    chrome.tabs.query( {active: true, currentWindow: true}, function(tabs) {
+        let activeTab = tabs[0];
+        leads.push(activeTab.url);
+        console.log(activeTab);
+        console.log(activeTab.url);
+        window.localStorage.setItem(leadsLocalStorageKey, JSON.stringify(leads));
+        render(leads);
+    } )
+})
+
+deleteBtn.addEventListener("dblclick", function() {
+    window.localStorage.clear();
+    leads = [];
+    render(leads);
+})
+
+function render(leads) {
     let listItems = ``;
-    leads.forEach(function (leadValue) {
-        listItems += `
-        <li>
-            <a target="_blank" href=${leadValue}>
-                ${leadValue}
-            </a>
-        </li>
-        `;
-    })
-    ulEl.innerHTML = listItems;
+    if (leads) {
+        leads.forEach(function (leadValue) {
+            listItems += `
+            <li>
+                <a target="_blank" href=${leadValue}>
+                    ${leadValue}
+                </a>
+            </li>
+            `;
+        })
+        ulEl.innerHTML = listItems;
+    }
+}
+
+function fetchLeads() {
+    const leadsFromLocalStorage = JSON.parse( window.localStorage.getItem(leadsLocalStorageKey) );
+    if (leadsFromLocalStorage) {
+        leads = leadsFromLocalStorage;
+        render(leads);
+    }
 }
 
 function clearIt(target) {
