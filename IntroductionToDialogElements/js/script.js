ef7ce09cdb8c9d22409c858692d902072b5a51d9
MehradMi <165255856+MehradMi@users.noreply.github.com>
2025-11-14 19:14:53 +0330

    Added IntroductionToDialogElements: Learning About Modals In JS

---
+++ IntroductionToDialogElements/js/script.js
@@ -0,0 +1,12 @@
+
+const dialog = document.getElementById("modal");
+const openButton = document.getElementById("open-modal-btn");
+const closeButton = document.getElementById("close-modal-btn");
+
+openButton.addEventListener("click", () => {
+  dialog.show();
+});
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
